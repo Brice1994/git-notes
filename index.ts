559ef5689fc3f677da73a9d034681b595b1f804d
brice <bricelehew@gmail.com>
2021-01-03 05:54:11 -0600

    save

---
+++ index.ts
@@ -0,0 +1,31 @@
+import yargs from "yargs";
+import {ChildProcessWithoutNullStreams, spawn} from "child_process";
+
+// const argv = yargs(process.argv.slice(2))
+// .option("f", {
+//     alias: "file",
+//     type: "string",
+//     nargs: 1,
+//     demand: true,
+// })
+// .argv;
+
+// console.log(argv.file);
+
+(async () => {
+    await spawnPromise(spawn("git", ["add", "."]));
+    await spawnPromise(spawn("git", ["commit", "-m", "save"]));
+})();
+async function spawnPromise(childProcess: ChildProcessWithoutNullStreams){
+    childProcess.stdout.on("data", (data) => {
+        console.log(`${childProcess.spawnfile}: ${data}`);
+    });
+    childProcess.stderr.on("data", (data) => {
+        console.error(`${childProcess.spawnfile}: ${data}`);
+    })
+    return await new Promise((res) => {
+        childProcess.on("close", (code) => {
+            res(code)
+        });
+    })
+}
